812d5859c97d43c6417ef69535a9b46e25cae559
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-09-13 12:18:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const makeChange = (c) => {
  let result = { q: 0, d: 0, n: 0, p: 0 };
-  result.q = Math.floor(amount / 25);
-  amount %= 25;
-  result.d = Math.floor(amount / 10);
-  amount %= 10;
-  result.n = Math.floor(amount / 5);
-  amount %= 5;
-  result.p = amount;
+  result.q = Math.floor(c / 25);
+  c %= 25;
+  result.d = Math.floor(c / 10);
+  c %= 10;
+  result.n = Math.floor(c/ 5);
+  c %= 5;
+  result.p = c;
 
-  return result;
+  return c;
 }
 
 // Do not the change the code below
-//const c = prompt("Enter c: ");
+const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
